629531f4fcd6efdc966da983962209b75a8614a1
OwenZhangSH <1915427675@qq.com>
2020-09-14 11:26:43 +0800

    Solutions of 300

---
+++ Array/300. Longest Increasing Subsequence/LengthOfLIS1.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int max = 0;
+        int size = nums.Length;
+        int[] currentMax = new int[size];
+        int current = 0;
+        if (size == 0)
+        {
+            return max;
+        }
+        max = 1;
+        currentMax[size - 1] = 1;
+        for (int i = size - 2; i >= 0; i--)
+        {
+            currentMax[i] = 1;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    currentMax[i] = currentMax[i] > (currentMax[j] + 1) ? currentMax[i] : (currentMax[j] + 1);
+                }
+            }
+            max = max > currentMax[i] ? max : currentMax[i];
+        }
+        return max;
+    }
+}
---
+++ Array/300. Longest Increasing Subsequence/LengthOfLIS2.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int len = 1;
+        int size = nums.Length;
+        if (size == 0) return 0;
+        int[] minLength = new int[size + 1];
+        minLength[len] = nums[0];
+        for (int i = 1; i < size; ++i)
+        {
+            if (nums[i] > minLength[len]) minLength[++len] = nums[i];
+            else
+            {
+                int l = 1, r = len, pos = 0;
+                while (l <= r)
+                {
+                    int mid = (l + r) >> 1;
+                    if (minLength[mid] < nums[i])
+                    {
+                        pos = mid;
+                        l = mid + 1;
+                    }
+                    else r = mid - 1;
+                }
+                minLength[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+}

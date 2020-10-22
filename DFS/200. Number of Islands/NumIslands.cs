4ffbf05c0660b64288954bef79b9b0755fa0d89e
OwenZhangSH <1915427675@qq.com>
2020-10-22 10:57:16 +0800

    update 200

---
+++ DFS/200. Number of Islands/NumIslands.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int NumIslands(char[][] grid)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+        int count = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    DFS(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void DFS(char[][] grid, int x, int y)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+
+        grid[x][y] = '0';
+        if (x - 1 >= 0 && grid[x - 1][y] == '1') DFS(grid, x - 1, y);
+        if (x + 1 < m && grid[x + 1][y] == '1') DFS(grid, x + 1, y);
+        if (y - 1 >= 0 && grid[x][y - 1] == '1') DFS(grid, x, y - 1);
+        if (y + 1 < n && grid[x][y + 1] == '1') DFS(grid, x, y + 1);
+    }
+}
---
+++ DFS/200. Number of Islands/README.md
@@ -0,0 +1,39 @@
+#### [200. Number of Islands](https://leetcode-cn.com/problems/number-of-islands/)
+
+## 1.题目
+
+Given an m x n 2d grid map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example1:
+
+```
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+```
+
+Example2:
+
+```
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+```
+
+
+
+# 2. 分析
+
+DFS递归的将周围的1变成0；
+
+遍历整个2维数组计算深度搜索的次数
